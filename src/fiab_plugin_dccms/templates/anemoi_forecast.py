903b391bceed67cadfee2da4150eb4f58d8fa186
Håvard Futsæter <havardf@met.no>
2026-09-23 11:58:53 +0100

    Correct checkpoint id

--- src/fiab_plugin_dccms/templates/anemoi_forecast.py
+++ src/fiab_plugin_dccms/templates/anemoi_forecast.py
@@ -38,7 +38,7 @@ template = BlueprintTemplate(
             factory_id=BlockFactoryId("anemoiSource"),
             instance=BlockInstance(
                 configuration_values={
-                    ConfigurationOptionId("checkpoint"): "BRIS-Malawi-2025.10",
+                    ConfigurationOptionId("checkpoint"): "ecmwf:aifs-global-o48",
                     ConfigurationOptionId("input_source"): "${initialConditions}",
                     ConfigurationOptionId("lead_time"): "72",
                     ConfigurationOptionId("base_time"): YESTERDAY_MIDNIGHT,
